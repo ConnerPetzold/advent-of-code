0c2ed5b8cd8fb3baa5f06cee562025bcf954a6db
Conner Petzold <conner@ersc.io>
2025-12-02 00:56:01 -0500

    day 2 part 2

--- 2025/src/day2.rs
+++ 2025/src/day2.rs
@@ -44,6 +44,34 @@ fn is_valid_id(id: u64) -> bool {
     left != right
 }
 
+#[aoc(day2, part2)]
+pub fn solve_part2(input: &Vec<Range<u64>>) -> u64 {
+    let mut invalid_ids = Vec::new();
+    for range in input.clone() {
+        for id in range {
+            if !is_valid_id_part2(id) {
+                invalid_ids.push(id);
+            }
+        }
+    }
+    invalid_ids.iter().sum()
+}
+
+fn is_valid_id_part2(id: u64) -> bool {
+    let s = id.to_string();
+    let len = s.len();
+    for n in 1..=len / 2 {
+        if len % n != 0 {
+            continue;
+        }
+        let sub = s.chars().take(n).collect::<String>();
+        if sub.repeat(len / n) == s {
+            return false;
+        }
+    }
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +81,12 @@ mod tests {
         let input = r"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         let input = input_generator(input);
         assert_eq!(solve_part1(&input), 1227775554);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        let input = input_generator(input);
+        assert_eq!(solve_part2(&input), 4174379265);
     }
 }
