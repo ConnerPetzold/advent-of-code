858acbf955a98b23b389c30c85de1176ae3efb3c
Conner Petzold <conner@ersc.io>
2025-12-04 00:00:00 -0500

    day 4 part 1

---
+++ 2025/src/day4.rs
@@ -0,0 +1,54 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use nom::{
+    Parser,
+    character::complete::{self, one_of},
+    multi::{many1, separated_list0},
+};
+
+type Grid = gridit::Grid<bool>;
+
+#[aoc_generator(day4)]
+pub fn input_generator(input: &str) -> Grid {
+    let rows = separated_list0(
+        complete::newline::<&str, nom::error::Error<&str>>,
+        many1(one_of(".@").map(|c| c == '@')),
+    )
+    .parse(input)
+    .unwrap()
+    .1;
+
+    let width = rows[0].len();
+    let height = rows.len();
+    let items = rows.iter().flatten().cloned().collect::<Vec<_>>();
+    Grid::from(items, width, height)
+}
+
+#[aoc(day4, part1)]
+pub fn solve_part1(grid: &Grid) -> usize {
+    grid.positions()
+        .filter(|&p| {
+            matches!(grid.get(p), Some(true)) && grid.neighbors(p).filter(|&&n| n).count() < 4
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = r"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        let grid = input_generator(input);
+        assert_eq!(solve_part1(&grid), 13);
+    }
+}
