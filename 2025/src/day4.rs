c5ee11ff774bf8d7e6c376be1f60e88b9529f68a
Conner Petzold <conner@ersc.io>
2025-12-04 00:21:28 -0500

    day 4 part 2

--- 2025/src/day4.rs
+++ 2025/src/day4.rs
@@ -23,13 +23,38 @@ pub fn input_generator(input: &str) -> Grid {
     Grid::from(items, width, height)
 }
 
-#[aoc(day4, part1)]
-pub fn solve_part1(grid: &Grid) -> usize {
+fn removable_positions(grid: &Grid) -> Vec<gridit::Position> {
     grid.positions()
         .filter(|&p| {
             matches!(grid.get(p), Some(true)) && grid.neighbors(p).filter(|&&n| n).count() < 4
         })
-        .count()
+        .collect()
+}
+
+#[aoc(day4, part1)]
+pub fn solve_part1(grid: &Grid) -> usize {
+    removable_positions(grid).len()
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(grid: &Grid) -> usize {
+    let mut total = 0;
+    let (width, height) = grid.size();
+    let items = grid.iter().cloned().collect::<Vec<_>>();
+    let mut grid = Grid::from(items, width, height);
+
+    loop {
+        let positions = removable_positions(&grid);
+        if positions.is_empty() {
+            break;
+        }
+
+        for position in positions.iter() {
+            grid.set(*position, false);
+        }
+        total += positions.len();
+    }
+    total
 }
 
 #[cfg(test)]
