1a5c41dfb294074f73ff148faf9f33b8b6ec304d
Conner Petzold <conner@ersc.io>
2025-12-05 00:18:56 -0500

    day 5 part 2

--- 2025/src/day5.rs
+++ 2025/src/day5.rs
@@ -1,6 +1,7 @@
 use std::ops::RangeInclusive;
 
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 use nom::{
     Parser,
     bytes::complete::tag,
@@ -38,6 +39,47 @@ pub fn solve_part1((ranges, numbers): &(Vec<RangeInclusive<u64>>, Vec<u64>)) -> 
         .count()
 }
 
+#[aoc(day5, part2)]
+pub fn solve_part2((ranges, _): &(Vec<RangeInclusive<u64>>, Vec<u64>)) -> usize {
+    ranges
+        .iter()
+        .sorted_by(|a, b| a.start().cmp(b.start()))
+        .fold(Vec::<RangeInclusive<u64>>::new(), |mut acc, range| {
+            let mut combined = false;
+            for r in acc.iter_mut() {
+                if r.overlaps(range) {
+                    *r = r.combine(range);
+                    combined = true;
+                    break;
+                }
+            }
+
+            if !combined {
+                acc.push(range.clone());
+            }
+
+            acc
+        })
+        .iter()
+        .map(|range| range.clone().count())
+        .sum()
+}
+
+trait RangeExt {
+    fn overlaps(&self, other: &RangeInclusive<u64>) -> bool;
+    fn combine(&self, other: &RangeInclusive<u64>) -> RangeInclusive<u64>;
+}
+
+impl RangeExt for RangeInclusive<u64> {
+    fn overlaps(&self, other: &RangeInclusive<u64>) -> bool {
+        self.start() <= other.end() && other.start() <= self.end()
+    }
+
+    fn combine(&self, other: &RangeInclusive<u64>) -> RangeInclusive<u64> {
+        *self.start().min(other.start())..=*self.end().max(other.end())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,5 +99,22 @@ mod tests {
 32";
         let input = input_generator(input);
         assert_eq!(solve_part1(&input), 3);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r"3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+        let input = input_generator(input);
+        assert_eq!(solve_part2(&input), 14);
     }
 }
