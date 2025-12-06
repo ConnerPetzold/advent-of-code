58ff8538a580dcb6db8865be5cf77eb2456c83b6
Conner Petzold <conner@ersc.io>
2025-12-06 00:22:18 -0500

    day 6 part 2

--- 2025/src/day6.rs
+++ 2025/src/day6.rs
@@ -27,8 +27,8 @@ impl Problem {
     }
 }
 
-#[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> Vec<Problem> {
+#[aoc_generator(day6, part1)]
+pub fn input_generator_part_1(input: &str) -> Vec<Problem> {
     let (nums, ops) = separated_pair(
         separated_list1(
             complete::newline::<&str, nom::error::Error<&str>>,
@@ -68,8 +68,57 @@ pub fn input_generator(input: &str) -> Vec<Problem> {
     problems
 }
 
+#[aoc_generator(day6, part2)]
+pub fn input_generator_part_2(input: &str) -> Vec<Problem> {
+    let lines = input
+        .lines()
+        .map(|line| line.to_string())
+        .collect::<Vec<_>>();
+    let (op_line, num_lines) = lines.split_last().unwrap();
+
+    let mut problems = Vec::new();
+    let mut p = Problem {
+        op: Operator::Add,
+        nums: Vec::new(),
+    };
+
+    for (i, c) in op_line.chars().enumerate() {
+        if let Some(op) = match c {
+            '+' => Some(Operator::Add),
+            '*' => Some(Operator::Multiply),
+            _ => None,
+        } {
+            if !p.nums.is_empty() {
+                problems.push(p.clone());
+            }
+            p = Problem {
+                op,
+                nums: Vec::new(),
+            };
+        }
+
+        let mut num_str = String::new();
+        for num_line in num_lines {
+            num_str.push(num_line.chars().nth(i).unwrap());
+        }
+        if let Ok(num) = num_str.trim().parse() {
+            p.nums.push(num);
+        }
+    }
+    if !p.nums.is_empty() {
+        problems.push(p.clone());
+    }
+
+    problems
+}
+
 #[aoc(day6, part1)]
 pub fn solve_part1(problems: &Vec<Problem>) -> u64 {
+    problems.iter().map(|problem| problem.solve()).sum()
+}
+
+#[aoc(day6, part2)]
+pub fn solve_part2(problems: &Vec<Problem>) -> u64 {
     problems.iter().map(|problem| problem.solve()).sum()
 }
 
@@ -83,7 +132,17 @@ mod tests {
  45 64  387 23 
   6 98  215 314
 *   +   *   +  ";
-        let input = input_generator(input);
+        let input = input_generator_part_1(input);
         assert_eq!(solve_part1(&input), 4277556);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r"123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  ";
+        let input = input_generator_part_2(input);
+        assert_eq!(solve_part2(&input), 3263827);
     }
 }
